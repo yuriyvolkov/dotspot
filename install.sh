ddd085128db33993123ac0c52c2682b4bbd9d118
user <jnvolkov@gmail.com>
2015-06-11 08:22:18 +0300

    install goodies and symlink configs to HOME dir

--- install.sh
+++ install.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
-for dotfile in $(find . -maxdepth 1 -type f -name .\*)
+TARGET="`pwd`"
+if [ -e $HOME/.vim ]
+then
+  echo "$HOME/.vim exists, skipping..."
+else
+  echo "$HOME/.vim does not exists, clonning"
+  cd $HOME && git clone git@github.com:yuriyvolkov/pingvim.git .vim
+fi
+
+if [ -e $HOME/.gitamins ]
+then
+  echo "$HOME/.gitamins exists, skipping..."
+else
+  echo "$HOME/.gitamins does not exists, install"
+  cd $HOME && git clone git@github.com:yuriyvolkov/gitamins.git .gitamins && cd .gitamins && ./install.sh
+fi
+
+if [ -e $HOME/.oh-my-zsh ]
+then
+  echo "$HOME/.oh-my-zsh exists, skipping..."
+else
+  echo "$HOME/.oh-my-zsh does not exists, install"
+  cd $HOME && git clone git@github.com:robbyrussell/oh-my-zsh.git .oh-my-zsh
+fi
+
+for dotfile in $(ls -a | grep -v .git | grep -e '^\.[a-z]\+' | tr '\n' ' ')
 do
   if [ -e $HOME/$dotfile ]
   then
-    echo "$HOME/$dotfile Exists, skipping..."
+    echo "$dotfile exists in $HOME"
   else
-    echo "synlinking $dotfile to $HOME/$dotfile"
-    ln -s $dotfile $HOME/$dotfile
+    echo "symlinking $TARGET/$dotfile to $HOME/$dotfile"
+    ln -s $TARGET/$dotfile $HOME/$dotfile
   fi
 done
